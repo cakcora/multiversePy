8dd4fb2ab7b99e54ef44b40228e947a1f0dc223b
jonbrownell <brownel4@myumanitoba.ca>
2021-06-11 16:35:05 -0500

    Fix poison, add requirements and .gitignore
    Poison function was crashing for large
    dataframes. This is fixed by poisoning a
    random sample of indices. The change also
    prevents a pandas warning.
    
    Also added a .gitignore and requirements.txt
    to hopefully make setup easier. :)

--- src/helper.py
+++ src/helper.py
@@ -13,7 +13,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn import preprocessing
 from sklearn import metrics
-import matplotlib.pyplot as plt
 import math
 import scipy.stats
 
@@ -26,7 +25,7 @@ def mineTrees(rf_model):
     for t in range(0, rf_model.n_estimators):
         # print("Tree " + str(t) + " is processing")
         tree = rf_model.estimators_[t]
-        graph = nx.DiGraph() # Multiple edges are not allowed
+        graph = nx.DiGraph()  # Multiple edges are not allowed
 
         # export_graphviz(tree, out_file=str('results/trees/tree') + str(t) + '.dot',
         # feature_names=dataTrain.columns,class_names=data2.Class,rounded=True,
@@ -44,7 +43,7 @@ def mineTrees(rf_model):
                     graph.add_edge(node, features[l_child])
                 if r_child > 0 and features[r_child] >= 0:
                     # print(str(t) + ">" + str(node) + " r" + str(r_child) + " " + str(features[r_child]))
-                    graph.add_edge(node, features[r_child]) #compare the graph with the original decision tree to make that the graph is correct
+                    graph.add_edge(node, features[r_child])  # compare the graph with the original decision tree to make that the graph is correct
 
         # Network metrics
         hubs, authorities = nx.hits_numpy(graph)
@@ -67,24 +66,41 @@ def mineTrees(rf_model):
     return result
 
 
-def poison(target_data, percentage, message=False):
+def poison(target_data, percentage, message=True):
+    """
+    poison a percentage of a dataframe (target_data) and return a new dataframe
+
+    :param target_data:     dataframe to poison
+    :param percentage:      percentage of dataframe to poison
+    :param message:         print a status message
+
+    :return:                poisoned dataframe
+    """
+    poisoned_data = target_data.copy()  # avoid poisoning original data
+
     if percentage > 0:
-        l = len(target_data.index)
-        end = int(percentage * l / 100)
+        length = len(poisoned_data.index)
+
+        num_to_poison = int(percentage * length / 100)
+
         if message:
-            print("out of " + str(l) + " rows, labels of " + str(end) + " will be flipped")
-        unique_vals = target_data.Class.unique()
+            print("out of " + str(length) + " rows, labels of " + str(num_to_poison) + " will be flipped")
+        unique_vals = poisoned_data.Class.unique()
+
         if len(unique_vals) <= 1:
             print("ERROR: Dataset contains a single label for the Class feature")
         else:
-            #don't poison from the beginning. pick i randomly and make sure you don't take same i two times
-            for i in range(1, end):
-                current_label = target_data.Class[i]
-                new_label = current_label
-                while new_label == current_label:
-                    new_label = str(random.choice(unique_vals))
-                target_data.Class[i] = new_label  # gives SettingWithCopyWarning
-    return target_data
+            # don't poison from the beginning. pick i randomly and make sure you don't take same i two times
+            for i in np.random.choice(poisoned_data.index.array, num_to_poison, replace=False):  # random indices
+                new_label = poisoned_data.Class[i]
+
+                while new_label == poisoned_data.Class[i]:
+                    new_label = random.choice(unique_vals)
+
+                poisoned_data.at[i, 'Class'] = new_label  # fixes SettingWithCopyWarning
+
+    return poisoned_data
+
 
 def conf_matrix(data, major_max, minor_max):
     n_est = 100
@@ -118,9 +134,8 @@ def conf_matrix(data, major_max, minor_max):
         print(matrix)
         entropy = scipy.stats.entropy(matrix)
         print(entropy)
-        #entropies = pd.DataFrame()
+        # entropies = pd.DataFrame()
 
-
-    sns.heatmap(matrix, annot=True, annot_kws={'size':10},
+    sns.heatmap(matrix, annot=True, annot_kws={'size': 10},
                 cmap=plt.cm.Greens, linewidths=0.2)
     plt.show()
