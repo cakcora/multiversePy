c1a745691a7de1168368e6e21b13acd3cf206374
azad-p <azadp@myumanitoba.ca>
2022-05-18 18:04:08 -0500

    saving the 5 most important features

--- src/xai.py
+++ src/xai.py
@@ -7,20 +7,17 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.inspection import permutation_importance
 from sklearn.preprocessing import OrdinalEncoder
-import pandas_profiling
 import cufflinks as cf
-from pdpbox import pdp, info_plots
-import random
-from sklearn.inspection import PartialDependenceDisplay
+from pdpbox import pdp
 cf.set_config_file(sharing='public',theme='pearl',offline=False)
 cf.go_offline()
-from sklearn.inspection import plot_partial_dependence
 
 
 configs_preprocessing = preprocess.get_configs()
 
 for i in range(len(configs_preprocessing)):
     conf = configs_preprocessing[i]
+    print(conf["name"])
     raw = pd.read_csv(conf["data_path"], sep=',', header=0)
     class_col = conf['class_column'] if conf['class_column'] != -1 else len(raw.columns) - 1
     raw.rename(columns={raw.columns[class_col]: 'Class'}, inplace=True)
@@ -40,6 +37,7 @@ for i in range(len(configs_preprocessing)):
     result = encoder.fit_transform(X)
     X_train_enc = encoder.transform(X_train)
     X_test_enc = encoder.transform(X_test)
+    X_train_enc_df = pd.DataFrame(X_train_enc)
     X_data = np.concatenate((X_train_enc,X_test_enc),0)
 
     forest = RandomForestClassifier(n_estimators=300)
@@ -50,38 +48,35 @@ for i in range(len(configs_preprocessing)):
     importances = forest.feature_importances_
     std = np.std([tree.feature_importances_ for tree in forest.estimators_], axis=0)
     forest_importances = pd.Series(importances, index=feature_names)
-    plt.figure(figsize=(12, 12))
     fig, ax = plt.subplots()
-    forest_importances.plot.bar(yerr=std, ax=ax)
+
+    forest_importances.plot.bar(yerr=std, ax=ax, figsize=(14, 12))
     ax.set_title("feature importances using MDI")
     ax.set_ylabel("Mean decrease in impurity")
-    # fig.tight_layout()
     plt.savefig('../results/feature_importance/' + f'feature_importance_{conf["name"]}.png')
+    sortedSeries = forest_importances.sort_values(ascending=False)
+    sortedSeries.drop(sortedSeries.index[5:], inplace=True)
+    sortedSeries.to_csv(f'../results/feature_importance/feature_importance_{conf["name"]}.csv', mode='w')
+
 
     result = permutation_importance(forest, X_test_enc, y_test, n_repeats=10, random_state=42, n_jobs=2)
     forest_importances = pd.Series(result.importances_mean, index=feature_names)
-
+    sortedPermutation = forest_importances.sort_values(ascending=False)
+    sortedPermutation.drop(sortedPermutation.index[5:], inplace=True)
+    sortedPermutation.to_csv(f'../results/feature_importance/permutation_importance_{conf["name"]}.csv', mode='w')
     fig, ax = plt.subplots()
-    plt.figure(figsize=(12, 12))
+    plt.figure(figsize=(14, 12))
     forest_importances.plot.bar(yerr=result.importances_std)
     plt.savefig('../results/feature_importance/' + f'permutation_importance_{conf["name"]}.png')
 
-    def pdp_isolate(feature):
-        pdp_age = pdp.pdp_isolate(
-            model=forest, dataset=X_train_enc, model_features=X_train.columns, feature=feature
-        )
-        # PDP Plot
-        fig, axes = pdp.pdp_plot(pdp_age, feature, plot_lines=False, center=False, frac_to_plot=0.5, plot_pts_dist=True,
-                                 x_quantile=True, show_percentile=True)
-        plt.savefig('../results/feature_importance/' + f'pdp_isolate_{conf["name"]}.png')
-
-
-    pdp_isolate(X.columns[0])
-
+    from sklearn.inspection import PartialDependenceDisplay
+    # def pdp_isolate(feature):
+    PartialDependenceDisplay.from_estimator(forest, X_data, [0, (0, 1)])
+    plt.savefig('../results/feature_importance/' + f'pdp_isolate_{conf["name"]}.png')
 
     def pdp_interact(feature1, feature2):
         inter1 = pdp.pdp_interact(
-            model=forest, dataset=X_train_enc, model_features=X_train.columns, features=[feature1, feature2]
+            model=forest, dataset=X_train_enc_df, model_features=X_train.columns, features=[feature1, feature2]
         )
         fig, axes = pdp.pdp_interact_plot(
             pdp_interact_out=inter1, feature_names=[feature1, feature2], plot_type='contour', x_quantile=False,
@@ -90,7 +85,6 @@ for i in range(len(configs_preprocessing)):
         axes['pdp_inter_ax'].set_yticklabels(feature1)
         axes['pdp_inter_ax'].set_xticklabels(feature2)
         plt.savefig('../results/feature_importance/' + f'pdp_interact_{conf["name"]}.png')
-
 
     pdp_interact(X.columns[0],X.columns[1])
 
