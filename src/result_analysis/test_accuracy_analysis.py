7bca9a2903f4fd528d737f0f124e210e38e8dc9b
azad-p <azadp@myumanitoba.ca>
2022-11-10 16:15:39 -0600

    accuracy plot

---
+++ src/result_analysis/test_accuracy_analysis.py
@@ -0,0 +1,17 @@
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+for file in os.listdir("../../results/accuracies/"):
+    print(file)
+    result = pd.read_csv("../../results/accuracies/" + file)
+    result_major = result[result['minor'] == 0]
+    result_major = result_major.drop("minor_train_accuracy", axis=1)
+    result_major = result_major.drop("minor", axis=1)
+    # print(result_major.head())
+
+    line_plot = sns.lineplot(data = result_major, x = 'major', y='minor_test_accuracy')
+    plt.legend(labels=["Adult",'BreastCancer','CarEvaluation',"HeartDisease","Nursery"])
+fig = line_plot.get_figure()
+fig.savefig('my_lineplot.png')
